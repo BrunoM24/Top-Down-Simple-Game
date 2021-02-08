591916000a7421980c86fea46e6ff2c10e9a3cd5
BrunoM24 <brunocostamatos.24@gmail.com>
2021-02-08 09:11:32 +0000

    Player - Implemented the Basic Movement Logic

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,2 +1,20 @@
 extends KinematicBody2D
 
+
+onready var sprite: Sprite = $Sprite
+
+export var max_speed := 200.0
+
+var _velocity := Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	var move := Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+	
+	_velocity += move.normalized() * max_speed
+	
+	_velocity = move_and_slide(_velocity)
+	
