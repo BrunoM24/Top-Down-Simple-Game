d6b420b7636f65b6586c66f9faa3cf3b45884467
BrunoM24 <brunocostamatos.24@gmail.com>
2021-02-11 17:56:00 +0000

    Items - Pistol - Implmented the Shoot Logic

--- src/items/guns/Pistol.gd
+++ src/items/guns/Pistol.gd
@@ -1,9 +1,16 @@
 extends Gun
 
 
+var bulletScene: PackedScene = preload("res://src/items/guns/Bullet.tscn")
+
+
 func unhandled_input(event: InputEvent) -> void:
-	print("Pistol shooting")
+	_fire()
 
 
 func _fire() -> void:
-	pass
+	var bullet : Area2D = bulletScene.instance()
+	add_child(bullet)
+	bullet.global_position = $BarrelPosition.global_position
+	bullet.direction = $BarrelPosition.global_position.direction_to(get_global_mouse_position())
+	bullet.look_at(bullet.direction + bullet.global_position)
