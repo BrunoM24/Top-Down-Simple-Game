2c5fdea5be1a58cc1875e77ddda2e459104bd8df
BrunoM24 <brunocostamatos.24@gmail.com>
2021-02-12 21:24:49 +0000

    Player - Added the Logic to pickup Items

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -53,6 +53,11 @@ func _movement_logi() -> void:
 func _interact() -> void:
 	if pickableObject:
 		print("pickup item")
+		gun = pickableObject
+		gun.get_parent().remove_child(gun)
+		$RightHand.add_child(gun)
+		gun.position = Vector2.ZERO
+		gun.equiped = true
 
 
 func _on_PickableArea_area_entered(area: Area2D) -> void:
