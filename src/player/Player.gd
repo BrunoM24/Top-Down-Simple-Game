2d8916e0f54a7d7acf910a60974da053ce0db4af
BrunoM24 <brunocostamatos.24@gmail.com>
2021-02-12 19:21:29 +0000

    Player - Added the Ability to detect pickable items

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 
 onready var sprite: Sprite = $Sprite
+onready var pickableArea: Area2D = $PickableArea
 
 export var max_speed := 200.0
 
@@ -9,6 +10,9 @@ var _velocity := Vector2.ZERO
 
 var gun: Gun
 var gunScene: PackedScene = preload("res://src/items/guns/Pistol.tscn")
+
+var pickableObjects: Array
+var pickableObject
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -19,9 +23,21 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.get_action_strength("left_trigger") && gun:
 		gun.unhandled_input(event)
 	
+	if event.is_action_pressed("interact"):
+		_interact()
+	
 
 
 func _physics_process(delta: float) -> void:
+	_movement_logi()
+	
+#	pickableObjects = pickableArea.get_overlapping_areas()
+#
+#	if pickableObjects.size() > 0:
+#		print(pickableObjects)
+
+
+func _movement_logi() -> void:
 	var move := Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
@@ -32,3 +48,19 @@ func _physics_process(delta: float) -> void:
 	_velocity = move_and_slide(_velocity)
 	
 	look_at(get_global_mouse_position())
+
+
+func _interact() -> void:
+	if pickableObject:
+		print("pickup item")
+
+
+func _on_PickableArea_area_entered(area: Area2D) -> void:
+	if !pickableObject:
+		pickableObject = area.get_parent()
+		pickableObject.pickable = true
+
+
+func _on_PickableArea_area_exited(area: Area2D) -> void:
+	area.get_parent().pickable = false
+	pickableObject = null
