19515df68c5092e0a343486ab82851f677601770
BrunoM24 <brunocostamatos.24@gmail.com>
2021-02-14 16:52:43 +0000

    Guns - SMG - Implemented the SMG Shooting Logic

--- src/items/guns/SMG.gd
+++ src/items/guns/SMG.gd
@@ -1,2 +1,18 @@
 extends Gun
 
+
+var can_shoot := true
+
+
+func _physics_process(delta: float) -> void:
+	if equiped && Input.is_action_pressed("left_trigger"):
+		_fire()
+	
+
+
+func _fire() -> void:
+	if can_shoot:
+		_spawn_bullet()
+		can_shoot = false
+		yield(get_tree().create_timer(0.1), "timeout")
+		can_shoot = true
