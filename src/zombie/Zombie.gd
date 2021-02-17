94512108b009e448dffd40438b1e4651e449daf4
BrunoM24 <brunocostamatos.24@gmail.com>
2021-02-17 14:49:25 +0000

    Zombie - Implemented the logic to emit the blood particles when the suffers damage

--- src/zombie/Zombie.gd
+++ src/zombie/Zombie.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
 
+export var blood_particles_scene: PackedScene
+
 export var max_health := 100
 
 var health := max_health
 
 
 func damage(value: int) -> void:
+	_spawn_blood_particles()
 	health -= value
 	if health < 1:
 		die()
@@ -14,3 +17,12 @@ func damage(value: int) -> void:
 
 func die() -> void:
 	queue_free()
+
+
+func _spawn_blood_particles() -> void:
+	if !blood_particles_scene:
+		return
+	
+	var blood_particle: Particles2D = blood_particles_scene.instance()
+	add_child(blood_particle)
+	blood_particle.emitting = true
